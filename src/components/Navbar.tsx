efbc9d9fb69019122754acd31296f9e6eba3afda
Wael-Morjen <waelmorjen@gmail.com>
2024-01-21 23:26:52 +0100

    secure login & register

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
-import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from "lucide-react";
 
 const Navbar = () => {
   return (
@@ -28,6 +29,11 @@ const Navbar = () => {
               })}>
                 Sign in
               </LoginLink>
+              <RegisterLink className={buttonVariants({
+                size: "sm"
+              })}>
+                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+              </RegisterLink>
             </>
           </div>
         </div>
